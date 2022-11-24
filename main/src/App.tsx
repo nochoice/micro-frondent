38ca9b77cae19e896e3080b1aa3677b00c751bdb
Roman Kora <roman.kora@tieto.com>
2022-11-24 14:44:21 +0100

    Added custom event for catching some events between mf

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -11,26 +11,30 @@ import UserMenu from 'user/UserMenu';
 
 console.log(UserMenu)
 
-const App = () => (
-  <div className="main">
-    <Suspense fallback="">
-      <Header title="Welcome" 
-        // left={<>left</>}
-        right={
-            <UserMenu />
-        }
-      />
-    </Suspense>
+const App = () => {
+  
+  document.addEventListener('menu-open', (e) => console.log(e.detail));
+  return (
+    <div className="main">
+      <Suspense fallback="">
+        <Header title="Welcome" 
+          // left={<>left</>}
+          right={
+              <UserMenu />
+          }
+        />
+      </Suspense>
 
-    
-    <section>
       
-    </section>
+      <section>
+        
+      </section>
 
-    <Suspense fallback="">
-      <Footer />
-    </Suspense>
-    
-  </div>
-);
+      <Suspense fallback="">
+        <Footer />
+      </Suspense>
+      
+    </div>
+  )
+      }
 ReactDOM.render(<App />, document.getElementById("app"));
--- user/src/components/user-menu/UserMenu.tsx
+++ user/src/components/user-menu/UserMenu.tsx
@@ -5,9 +5,21 @@ import './UserMenu.scss';
 const UserMenu = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
 
+    const handleOpeninig = () => {
+        setIsOpen(!isOpen);
+
+        const event = new CustomEvent("menu-open", {
+            detail: {
+              isOpen: !isOpen
+            }
+        });
+
+        document.dispatchEvent(event);
+
+    }
     return (
         <div className='user-menu'>
-            <button className="action" onClick={() => setIsOpen(!isOpen)}>Profile</button>
+            <button className="action" onClick={handleOpeninig}>Profile</button>
             {isOpen && (
                 <ul className="menu" >
                     <li>Profile</li>
